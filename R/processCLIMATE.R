d3b3ff971bd46984a700c5cdf59cb394801ef42f
Sourav Das <57733525+ronysd@users.noreply.github.com>
2025-04-30 11:58:49 -0400

    Update processCLIMATE.R

--- R/processCLIMATE.R
+++ R/processCLIMATE.R
@@ -19,12 +19,13 @@ processCLIMATE <- function(url, studyAreaRas) {
         grepl("^Normal_1991_2020_", names_b),
         {
           cleaned_name <- gsub("^Normal_1991_2020_", "", names_b)
-          cleaned_name <- gsub("(Tave|PPT)_(wt|sm|sp|at)", "\\1\\2", cleaned_name)  # Remove underscores
-          ifelse(grepl("_1km$", cleaned_name), cleaned_name, paste0(cleaned_name, "_1km"))  # Add _1km if needed
+          cleaned_name <- gsub("(Tave|PPT|DD)_([a-z0-9]+)", "\\1\\2", cleaned_name)  # Remove underscore in patterns like DD_0
+          ifelse(grepl("_1km$", cleaned_name), cleaned_name, paste0(cleaned_name, "_1km"))  # Add _1km if missing
         },
         # For other files, append _1km only if not already present
-        ifelse(grepl("_1km", names_b), names_b, paste0(names_b, "_1km"))
+        ifelse(grepl("_1km$", names_b), names_b, paste0(names_b, "_1km"))
       )
+      
       names(b) <- names_b
       b
     }),

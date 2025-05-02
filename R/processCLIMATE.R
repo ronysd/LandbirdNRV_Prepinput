b308820701fd9234cb9dd1b1349f0762b799b053
Sourav Das <57733525+ronysd@users.noreply.github.com>
2025-05-02 12:32:33 -0400

    Update processCLIMATE.R

--- R/processCLIMATE.R
+++ R/processCLIMATE.R
@@ -5,6 +5,8 @@ processCLIMATE <- function(url, studyAreaRas) {
   dPath <- "~/tmp/"
   dir.create(dPath, showWarnings = FALSE, recursive = TRUE)
   
+  message("Loading climate data from: ", url)
+  
   ras <- prepInputs(
     url = url,
     fun = quote({
@@ -17,11 +19,11 @@ processCLIMATE <- function(url, studyAreaRas) {
         grepl("^Normal_1991_2020_", names_b),
         {
           cleaned_name <- gsub("^Normal_1991_2020_", "", names_b)
-          cleaned_name <- gsub("(Tave|PPT|DD)_([a-z0-9]+)", "\\1\\2", cleaned_name)  
+          cleaned_name <- gsub("(Tave|PPT|DD)_([a-z0-9]+)", "\\1\\2", cleaned_name)  # Remove underscore in patterns like DD_0
           ifelse(grepl("_1km$", cleaned_name), cleaned_name, paste0(cleaned_name, "_1km"))  # Add _1km if missing
         },
-        # For other files, append _1km only if not already present
-        ifelse(grepl("_1km$", names_b), names_b, paste0(names_b, "_1km"))
+        # For other files, leave the name as it is (i.e. for climate annual process rasters)
+        names_b
       )
       
       names(b) <- names_b
